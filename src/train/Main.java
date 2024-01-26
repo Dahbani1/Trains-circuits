bdecca6422e44ac61bfeb2b2335b6ea1fa5d1c4f
Ezzakri Anas <62079515+ezzakri-anas@users.noreply.github.com>
2024-01-26 18:31:06 +0100

    Added sleep

--- src/train/Main.java
+++ src/train/Main.java
@@ -5,8 +5,8 @@ package train;
  */
 public class Main {
 	public static void main(String[] args) {
-		Station A = new Station("GareA", 3, 3);
-		Station D = new Station("GareD", 3, 0);
+		Station A = new Station("GareA", 10, 10);
+		Station D = new Station("GareD", 10, 0);
 		Section AB = new Section("AB");
 		Section BC = new Section("BC");
 		Section CD = new Section("CD");
@@ -15,25 +15,26 @@ public class Main {
 		System.out.println("\t" + r);
 		Position p = new Position(A, Direction.LR);
 		try {
-			Train t1 = new Train("1", p);
-			Train t2 = new Train("2", p);
-			Train t3 = new Train("3", p);
-			System.out.println(t1);
-			System.out.println(t2);
-			System.out.println(t3);
-			Thread tr1=new Thread(t1);
-			Thread tr2=new Thread(t2);
-			Thread tr3=new Thread(t3);
-			tr1.start();
-			tr2.start();
-			tr3.start();
+//			Train t1 = new Train("1", p);
+//			Train t2 = new Train("2", p);
+//			Train t3 = new Train("3", p);
+//			System.out.println(t1);
+//			System.out.println(t2);
+//			System.out.println(t3);
+//			Thread tr1=new Thread(t1);
+//			Thread tr2=new Thread(t2);
+//			Thread tr3=new Thread(t3);
+//			tr1.start();
+//			tr2.start();
+//			tr3.start();
+		    for (int i = 1; i <= 10; i++) {
+		        Train t = new Train(Integer.toString(i), p);
+		        System.out.println(t);
+		        Thread tr = new Thread(t);
+		        tr.start();
+		    }
 		} catch (BadPositionForTrainException e) {
 			System.out.println("Le train " + e.getMessage());
 		}
-		
-		
-		
-	
-
 	}
 }
